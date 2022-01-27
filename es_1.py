3c48c2f1582380781d4cf233c7773f496feff3a7
andrea-sacchetto <74718356+Andrea46-pixel@users.noreply.github.com>
2022-01-27 12:40:55 +0100

    First upload

---
+++ es_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+input = int(input("Inserisci un numero: "))
+
+if input>0:
+    print(f"Il numero successivo di {input} è {input+1}")
+else: 
+    print("N deve essere maggiore di 0")
+
+
